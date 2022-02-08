06bccf726dfa8d6ce281266d5f4fc963c5bef2f7
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-02-08 11:38:53 +0530

    Changes1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="Navbar">
+        <div className="one">
+          Home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
